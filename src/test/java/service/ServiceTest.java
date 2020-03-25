b9bbe9c845a152935cce7432c4b1ce943e24d35a
ian.chelaru <ianchelaru@yahoo.com>
2020-03-25 10:31:41 +0200

    Initial commit

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,33 @@
+package service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void saveStudent()
+    {
+        int a = 3 + 2;
+        assertEquals(5, a);
+    }
+
+    @Test
+    public void saveTema()
+    {
+    }
+}
